f7f5ef24c2b2cb14de18ba47b3bd459991e81f45
Camillo Bucciarelli <camillo.bucciarelli@nttdata.com>
2022-07-22 11:29:56 +0200

    feat: refine style

--- lib/data/data_source/hives_data_source.dart
+++ lib/data/data_source/hives_data_source.dart
@@ -7,12 +7,12 @@ import '../entities/hive_entity.dart';
 
 part 'hives_data_source.g.dart';
 
-@RestApi(baseUrl: Env.baseUrl)
+@RestApi()
 @lazySingleton
 abstract class HivesDataSource {
 
   @factoryMethod
-  factory HivesDataSource(Dio dio) = _HivesDataSource;
+  factory HivesDataSource(Dio dio) => _HivesDataSource(dio, baseUrl: Env.baseUrl);
 
   @GET('/hives')
   Future<List<HiveEntity>> getHives(@Query('honey_id') int honeyId);
--- lib/presentation/hives/widgets/hive_card.dart
+++ lib/presentation/hives/widgets/hive_card.dart
@@ -1,37 +1,86 @@
+import 'package:bee/presentation/hives/widgets/profile_image.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../domain/models/hive.dart';
 
-class HiveCard extends StatelessWidget {
+class HiveCard extends StatefulWidget {
   final Hive hive;
+
   const HiveCard(this.hive, {Key? key}) : super(key: key);
 
   @override
+  State<HiveCard> createState() => _HiveCardState();
+}
+
+class _HiveCardState extends State<HiveCard> with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Container(
+      height: 200,
+      width: double.infinity,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.amber.shade600.withOpacity(0.3),
+            blurRadius: 15,
+            offset: const Offset(0, 10),
+          ),
+        ],
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
+      child: Stack(
         children: [
-          if(hive.mainImage != null) _image(),
-          if(hive.name != null) Text(hive.name!, style: Theme.of(context).textTheme.headline6),
+          _image(),
+          _name(),
         ],
       ),
     );
   }
 
-  Widget _image(){
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: CachedNetworkImage(
-        imageUrl: hive.mainImage!,
-        fit: BoxFit.cover,
+  Widget _image() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: ShaderMask(
+        shaderCallback: (rect) => LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: const [.4, 1.0],
+          colors: [
+            Colors.transparent,
+            Colors.black.withOpacity(0.9),
+          ],
+        ).createShader(rect),
+        blendMode: BlendMode.darken,
+        child: CachedNetworkImage(
+          imageUrl: widget.hive.mainImage,
+          width: double.infinity,
+          fit: BoxFit.fitWidth,
+        ),
       ),
     );
   }
+
+  Widget _name() {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          widget.hive.name.toUpperCase(),
+          textAlign: TextAlign.right,
+          style: Theme
+              .of(context)
+              .textTheme
+              .headline5
+              ?.copyWith(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
